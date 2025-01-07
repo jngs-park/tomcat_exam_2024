3484f1921148d2930d248f6303d3c65aa24e6a6f
jngs-park <dear.jayp@gmail.com>
2025-01-07 09:47:39 +0900

    구구단 서블릿 추가

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -18,6 +18,11 @@ public class GugudanServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-로 하겠다.
         resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8 이다 라고 알리는 의미
 
-        resp.getWriter().append("안녕1111");
+        int dan = Integer.parseInt(req.getParameter("dan"));
+        int limit = Integer.parseInt(req.getParameter("limit"));
+        resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+        for (int i = 1; i <= limit; i++) {
+            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+        }
     }
 }
