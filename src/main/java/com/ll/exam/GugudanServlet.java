be9310fb50fe758f1baf6caafefc4d03590784e6
jngs-park <dear.jayp@gmail.com>
2025-01-07 10:52:27 +0900

    req와 resp를 직접 다루기 귀찮으니, Rq 도입

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -13,16 +13,15 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Rq rq = new Rq(req, resp);
 
-        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8 로 해석하겠다.
-        resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-로 하겠다.
-        resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8 이다 라고 알리는 의미
+        int dan = rq.getIntParam("dan", 0);
+        int limit = rq.getIntParam("limit", 0);
 
-        int dan = Integer.parseInt(req.getParameter("dan"));
-        int limit = Integer.parseInt(req.getParameter("limit"));
-        resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
+        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+
         for (int i = 1; i <= limit; i++) {
-            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
         }
     }
 }
